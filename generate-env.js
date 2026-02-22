f9d9f38ffdb323713bfcc90f8a586936864aaf33
Joren VANDERZANDE <joren.vanderzande@soprasteria.com>
2026-02-22 15:14:25 +0100

    fix

--- generate-env.js
+++ generate-env.js
@@ -4,7 +4,7 @@ const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
 
 // Try to load .env file for local development (won't exist on Vercel)
-require('dotenv').config({path: path.join(__dirname, '.env')});
+require('dotenv').config({path: path.join(__dirname, 'src', '.env')});
 
 const envFile = `export const environment = {
     SUPABASE_URL: '${process.env.SUPABASE_URL}',
@@ -12,7 +12,7 @@ const envFile = `export const environment = {
     SUPABASE_ANON_KEY: '${process.env.SUPABASE_ANON_KEY}',
 };
 `;
-const targetPath = path.join(__dirname, './environments/environment.development.ts');
+const targetPath = path.join(__dirname, 'src', 'environments', 'environment.development.ts');
 fs.writeFile(targetPath, envFile, (err) => {
     if (err) {
         console.error(err);
